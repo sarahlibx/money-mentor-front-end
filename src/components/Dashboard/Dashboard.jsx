14f7a7eb454579cf9d3cccd9a374af02d9298eb2
Sarah Smith <sarahlib90@gmail.com>
2026-01-20 19:32:29 -0500

    Added useEffect for future transaction summary rendering on dashboard.jsx

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,15 +1,22 @@
-import { useContext, useEffect, useState } from 'react'
-import { UserContext } from '../../contexts/UserContext'
-import * as userService from '../../services/userService'
+import { use, useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { UserContext } from '../../contexts/UserContext';
+import * as userService from '../../services/userService';
+// import transactionService for quick summary view of last 5 transactions/current balance
+import * as transactionService from '../../services/transactionService';
 
 const Dashboard = () => {
     const { user } = useContext(UserContext)
-    const [users, setUsers] = useState([])
+    const [users, setUsers] = useState([]);
+    const [recentTransactions, setRecentTransactions] = useState([]);
+
     useEffect(() => {
         const fetchUsers = async () => {
             try {
                 const fetchedUsers = await userService.index()
                 setUsers(fetchedUsers)
+                const transData = await transactionService.getRecent();
+                setRecentTransactions(transData);
             } catch (err) {
                 console.log(err)
             }
@@ -19,11 +26,25 @@ const Dashboard = () => {
 
     return (
         <main>
+            <>
             <h1>Welcome, {user.username}</h1>
             {users.map((item) => (
                 <p key={item._id}>{item.username}</p>
             ))}
+            <section>
+            <h2>Here are your recent money moves.</h2>
+                <ul>
+                    {recentTransactions.map((transaction) => {
+                        <li key={transaction._id}>
+                            {transaction.description}: ${transaction.amount} ({transaction.category?.name});
+                        </li>
+                    })}
+                </ul>
+                <Link to ='/transactions'>View All Transactions</Link>
+            </section>
+            </>
         </main>
-    )
-}
-export default Dashboard
+    );
+};
+
+export default Dashboard;
