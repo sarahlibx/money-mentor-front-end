1ba0cfd0baed7947522a43d363527497771561c4
Sarah Smith <sarahlib90@gmail.com>
2026-01-26 13:11:41 -0500

    added github link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,12 +9,13 @@ const Footer = () => {
                     <p>Smart tracking for smarter savings.</p>
                 </div>
                 <div className='footer-links'>
+                    <a href="https://github.com/angelikakasia/money-mentor-front-end" target="_blank">GitHub</a>
                     <a href="/about">About</a>
                     <a href="/privacy">Privacy</a>
                     <a href="/contact">Support</a>
                 </div>
                 <div className='footer-bottom'>
-                    <p>&copy {currentYear} Money Mentor. All rights reserved.</p>
+                    <p>&copy; {currentYear} Money Mentor. All rights reserved.</p>
                     <p>Made with 💚 by Gabriel, Angelika & Sarah.</p>
                 </div>
             </div>
